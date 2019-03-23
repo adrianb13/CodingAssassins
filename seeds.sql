329fb6ee9acd81f24ca81e758b47037a4bce63e4
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-03-23 13:26:12 -0700

    updated

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,4 @@
-USE developersdb;
+USE developerdb;
 
 ALTER TABLE developers CHANGE COLUMN createdAt createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE developers CHANGE COLUMN updatedAt updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
@@ -6,7 +6,7 @@ ALTER TABLE developers CHANGE COLUMN updatedAt updatedAt datetime NOT NULL DEFAU
 ALTER TABLE clients CHANGE COLUMN createdAt createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE clients CHANGE COLUMN updatedAt updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
-INSERT INTO developers(name, experience, cost_to_hire, pin)
+INSERT INTO developers(name, experience, cost_to_hire, password)
 VALUES("Fredrick", "current student", 800, 1234),
     ("Larry", "I've made a website before", 200, 1232),
     ("Lorietta", "One year experience in CSS and HTML", 400, 1232),
@@ -29,3 +29,7 @@ VALUES("Jacob", 6786786677, "Need an app made", "App needs to be able to help lo
     ("Carl", 8012329876, "I need a simple game", "Get creative, impress me"),
     ("Hob", 9210238769, "Need a monopoly type game", "Need the game to be like monopoly, but doesn't have to be."),
     ("Kyle", 2794037282, "Need a website of books", "I'd like to get the website to read out loud the books and turn the page when it's done.");
+    
+USE developerdb;
+SELECT * FROM developers;
+SELECT * FROM clients;
